370acca7f811c98797131d4d8aa90bd0056a465b
Marah Toubal <maraht13@meet.mit.edu>
2014-03-31 18:20:14 +0300

    main_game

--- buttons.py
+++ buttons.py
@@ -5,7 +5,7 @@ class button(object):
 		self.location = location
 		self.color = color
 	def draw(self,main_screen):
-		button_rec = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1])
+		self.rec = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1])
 		button_sq = pygame.Surface([self.size[0],self.size[1] ])
 		button_sq.fill(self.color)
-		main_screen.blit(button_sq, button_rec)
+		main_screen.blit(button_sq, self.rec)
